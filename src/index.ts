403944c258fa47f76d9422d0eb08cd84d6ed3140
Martin Kopeček <github@smallhill.cz>
2020-08-13 10:05:21 +0200

    Do not output events after error

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,8 @@ export class SaxesStream extends Transform {
 
   path: string;
 
+  errored: boolean;
+
   constructor(saxesOptions: SaxesOptions = {}, streamOptions: TransformOptions = {}) {
 
     const defaultStreamOptions: TransformOptions = {
@@ -26,26 +28,36 @@ export class SaxesStream extends Transform {
     super(streamOptions);
 
     this.path = "";
+    this.errored = false;
 
     this.parser = new SaxesParser(saxesOptions);
 
     this.parser.onopentag = (tag) => {
-      this.path = [this.path, tag.name].join(".");
-      this.push({ path: this.path, event: "opentag", tag })
+      if (!this.errored) {
+        this.path = [this.path, tag.name].join(".");
+        this.push({ path: this.path, event: "opentag", tag })
+      }
     }
 
     this.parser.ontext = (text) => {
-      this.push({ path: this.path, event: "text", text })
+      if (!this.errored) {
+        this.push({ path: this.path, event: "text", text })
+      }
     }
 
     this.parser.onclosetag = (tag) => {
-      this.push({ path: this.path, event: "closetag", tag })
-      this.path = this.path.replace(/\.[^\.]+$/u, "");
+      if (!this.errored) {
+        this.push({ path: this.path, event: "closetag", tag })
+        this.path = this.path.replace(/\.[^\.]+$/u, "");
+      }
     }
 
     this.parser.onerror = (err) => {
-      this.emit("error", err)
+      this.errored = true;
+      this.emit("error", err);
     }
+
+
   }
 
   push(chunk: SaxesStreamChunk, encoding?: string) {
